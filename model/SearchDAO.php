526454a685643b9c93fba274777e0846f6e84a83
vasilensotirov <vasilensotirov@gmail.com>
2019-12-30 17:03:53 +0200

    search fix

--- model/SearchDAO.php
+++ model/SearchDAO.php
@@ -13,7 +13,7 @@ class SearchDAO{
         $sql = "SELECT v.id, v.title, v.date_uploaded, u.username, v.thumbnail_url FROM videos AS v 
                     JOIN users AS u ON v.owner_id = u.id WHERE v.title = ?;";
         $stmt = $pdo->prepare($sql);
-        $stmt->execute(array($search_query, $search_query, $search_query));
+        $stmt->execute(array($search_query));
         $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
         if (empty($rows)) {
             return false;
