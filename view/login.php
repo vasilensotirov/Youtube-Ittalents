78a04dd7affb3f197e3afb2d40395d5034d488f0
vasilensotirov <vasilensotirov@gmail.com>
2020-01-05 19:23:22 +0200

    added style for login

--- view/login.php
+++ view/login.php
@@ -5,25 +5,26 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/login.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <title>Youtube</title>
 </head>
 <body>
 <form action="index.php?target=user&action=login" method="post">
-    <table>
-        <tr>
-            <td><label for="email"><b>Email:</b></label></td>
-            <td><input type="email" id="email" name="email" required></td>
-        </tr>
-        <tr>
-            <td><label for="password"><b>Password:</b></label></td>
-            <td><input type="password" id="password" name="password" required></td>
-        </tr>
-        <tr>
-            <td><input type="submit" value="Login" name="login"></td>
-        </tr>
-    </table>
-</form>
-<button><a href='index.php?view=register'>Register</a></button>
+    <div class="wrapper fadeInDown">
+        <div id="formContent">
+            <div class="fadeIn first">
+                <img src="styles/images/youtube_PNG5.png" id="icon" alt="User Icon" />
+            </div>
+            <form action="index.php?target=user&action=login" method="post">
+                <input type="text" id="login" class="fadeIn second" name="email" placeholder="email">
+                <input type="password" id="password" class="fadeIn third" name="password" placeholder="password">
+                <input type="submit" class="fadeIn fourth" name="login" value="Log In">
+            </form>
+            <div id="formFooter">
+                Don't have an account? <a class="underlineHover" href='index.php?view=register'>Sign Up</a>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
