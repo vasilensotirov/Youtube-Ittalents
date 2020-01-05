315baffa414c5404ae9c1b461ddb8ae76b373ecc
vasilensotirov <vasilensotirov@gmail.com>
2020-01-05 19:47:57 +0200

    register styling added

--- view/register.php
+++ view/register.php
@@ -5,41 +5,56 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="styles/style.css">
-    <title>Document</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+    <title>Youtube</title>
 </head>
 <body>
-<form action="index.php?target=user&action=register" method="post" enctype="multipart/form-data">
-<table>
-    <tr>
-        <td><label for="username"><b>Username:</b></label></td>
-        <td><input type="text" id="username" name="username" required></td>
-    </tr>
-    <tr>
-        <td><label for="email"><b>Email:</b></label></td>
-        <td><input type="email" id="email" name="email" required></td>
-    </tr>
-    <tr>
-        <td><label for="password"><b>Password:</b></label></td>
-        <td><input type="password" id="password" name="password" required></td>
-    </tr>
-    <tr>
-        <td><label for="cpassword"><b>Confirm password:</b></label></td>
-        <td><input type="password" id="cpassword" name="cpassword" required></td>
-    </tr>
-    <tr>
-        <td><label for="full_name"><b>Full name:</b></label></td>
-        <td><input type="text" id="full_name" name="full_name" required></td>
-    </tr>
-    <tr>
-        <td><label for="avatar"><b>Add avatar</b></label></td>
-        <td><input type="file" name="avatar" id="avatar"</td>
-    </tr>
-    <tr>
-        <td><input type="submit" name="register" value="Register"></td>
-    </tr>
-</table>
-</form>
-<button><a href="index.php?view=login">Login here</a></button>
+<div class="container">
+<div class="row justify-content-center">
+    <div class="col-md-6">
+        <div class="card">
+            <header class="card-header">
+                <a href="index.php?view=login" class="float-right btn btn-outline-primary mt-1">Log in</a>
+                <h4 class="card-title mt-2">Sign up</h4>
+            </header>
+            <article class="card-body">
+                <form action="index.php?target=user&action=register" method="post" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label>Username</label>
+                        <input type="text" name="username" class="form-control" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label>Email address</label>
+                        <input type="email" name="email" class="form-control" placeholder="">
+                        <small class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" name="password" class="form-control" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label>Confirm password</label>
+                        <input type="password" name="cpassword" class="form-control" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label>Full name</label>
+                        <input class="form-control" type="text" name="full_name">
+                    </div>
+                    <div class="form-group">
+                        <label>Add avatar</label>
+                        <input class="form-control" type="file" name="avatar">
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" name="register" class="btn btn-primary btn-block"> Register  </button>
+                    </div>
+                    <small class="text-muted">By clicking the 'Sign Up' button, you confirm that you accept our <br> Terms of use and Privacy Policy.</small>
+                </form>
+            </article>
+            <div class="border-top card-body text-center">Already have an account? <a href="index.php?view=login">Log In</a></div>
+        </div>
+    </div>
+</div>
+</div>
 </body>
 </html>
