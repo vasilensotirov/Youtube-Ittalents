cc259ba2d86b2c8ba2830de874b6a0d24ef4ea82
Anna Voinova <vojnova@abv.bg>
2020-01-15 16:05:29 +0200

    video thumbnail file validation

--- controller/fileHandler.php
+++ controller/fileHandler.php
@@ -19,3 +19,20 @@ function uploadVideo ($file, $username){
     }
     return false;
 }
+
+function uploadImage($file, $username){
+    if (is_uploaded_file($_FILES[$file]["tmp_name"])) {$finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mime = finfo_file($finfo, $_FILES[$file]["tmp_name"]);
+        if (!(in_array($mime, array ('image/bmp', 'image/jpeg', 'image/png')))){
+            throw new InvalidFileException ("File is not in supported format.");
+        }
+        $file_name_parts = explode(".", $_FILES[$file]["name"]);
+        $extension = $file_name_parts[count($file_name_parts) - 1];
+        $filename = $username . "-" . time() . "." . $extension;
+        $file_url = "uploads" . DIRECTORY_SEPARATOR . $filename;
+        if (move_uploaded_file($_FILES[$file]["tmp_name"], $file_url)){
+            return $file_url;
+        }
+    }
+    return false;
+}
